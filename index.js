e93db58a61bfe0cd4794fb3113c948af079e1b43
sudhirVoid <bhandarisudhir4274@gmail.com>
2023-12-20 15:02:49 +0530

    sjdfsl

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const ejs = require('ejs');
-const path = require('path');
 
 const app = express();
-const port = 3000;
 
-// Set EJS as the view engine
-// app.set('view engine', 'ejs');
-// Set the views directory
-// app.set('views', path.join(__dirname, 'views'));
 
-// Define a route for the home page
+// Define a simple route
 app.get('/', (req, res) => {
-  // Render the 'index' template
-  res.send("<html> <head>server Response</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>");
+  // Render the 'index' view with a variable
+  res.send("REsponse from get")
 });
 
-// Start the server
+// Start the server on port 3000
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
