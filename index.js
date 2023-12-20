2adfc6e0574e2f549e920cfaf3b82dee5d441670
sudhirVoid <bhandarisudhir4274@gmail.com>
2023-12-20 15:19:36 +0530

    deplouyging

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const ejs = require('ejs');
-
+const createError = require('http-errors');
 const app = express();
-
 
 // Define a simple route
 app.get('/', (req, res) => {
   // Render the 'index' view with a variable
-  res.send("REsponse from get")
+  res.send("Response from get");
+});
+
+// Updated error handler to send a plain text error message
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.log(err.message);
+  res.status(err.status || 500).send('Error: ' + err.message);
+});
+
+// Middleware to handle 404 errors
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
 // Start the server on port 3000
